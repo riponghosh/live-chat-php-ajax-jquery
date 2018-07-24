94abc8c3aa7ff538b7f90d4eca533980580870bd
riponghosh <ripon.ghosh200@gmail.com>
2018-07-24 23:46:41 +0600

    insert chat message

--- index.php
+++ index.php
@@ -97,6 +97,21 @@ $(document).ready(function(){
   });
   $('#user_dialog_'+to_user_id).dialog('open');
  });
+
+ $(document).on('click', '.send_chat', function(){
+  var to_user_id = $(this).attr('id');
+  var chat_message = $('#chat_message_'+to_user_id).val();
+  $.ajax({
+   url:"insert_chat.php",
+   method:"POST",
+   data:{to_user_id:to_user_id, chat_message:chat_message},
+   success:function(data)
+   {
+    $('#chat_message_'+to_user_id).val('');
+    $('#chat_history_'+to_user_id).html(data);
+   }
+  })
+ });
  
 });  
 </script>
