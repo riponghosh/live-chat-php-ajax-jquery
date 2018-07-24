3c25beb5e991001f514861af3053636894ead875
riponghosh <ripon.ghosh200@gmail.com>
2018-07-24 23:45:12 +0600

    diglog box

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<!--
+//index.php
+!-->
+
 <?php
 
 include('database_connection.php');
@@ -30,6 +34,7 @@ if(!isset($_SESSION['user_id']))
     <h4 align="center">Online User</h4>
     <p align="right">Hi - <?php echo $_SESSION['username'];  ?> - <a href="logout.php">Logout</a></p>
     <div id="user_details"></div>
+    <div id="user_model_details"></div>
    </div>
   </div>
     </body>  
@@ -69,6 +74,29 @@ $(document).ready(function(){
    }
   })
  }
+
+ function make_chat_dialog_box(to_user_id, to_user_name)
+ {
+  var modal_content = '<div id="user_dialog_'+to_user_id+'" class="user_dialog" title="You have chat with '+to_user_name+'">';
+  modal_content += '<div style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;" class="chat_history" data-touserid="'+to_user_id+'" id="chat_history_'+to_user_id+'">';
+  modal_content += '</div>';
+  modal_content += '<div class="form-group">';
+  modal_content += '<textarea name="chat_message_'+to_user_id+'" id="chat_message_'+to_user_id+'" class="form-control"></textarea>';
+  modal_content += '</div><div class="form-group" align="right">';
+  modal_content+= '<button type="button" name="send_chat" id="'+to_user_id+'" class="btn btn-info send_chat">Send</button></div></div>';
+  $('#user_model_details').html(modal_content);
+ }
+
+ $(document).on('click', '.start_chat', function(){
+  var to_user_id = $(this).data('touserid');
+  var to_user_name = $(this).data('tousername');
+  make_chat_dialog_box(to_user_id, to_user_name);
+  $("#user_dialog_"+to_user_id).dialog({
+   autoOpen:false,
+   width:400
+  });
+  $('#user_dialog_'+to_user_id).dialog('open');
+ });
  
 });  
 </script>
