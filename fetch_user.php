424509348c49deefa52c5570994d4d2729eb147c
riponghosh <ripon.ghosh200@gmail.com>
2018-07-24 23:43:52 +0600

    fetch user data show

--- fetch_user.php
+++ fetch_user.php
@@ -28,10 +28,22 @@ $output = '
 
 foreach($result as $row)
 {
+ $status = '';
+ $current_timestamp = strtotime(date("Y-m-d H:i:s") . '- 10 second');
+ $current_timestamp = date('Y-m-d H:i:s', $current_timestamp);
+ $user_last_activity = fetch_user_last_activity($row['user_id'], $connect);
+ if($user_last_activity > $current_timestamp)
+ {
+  $status = '<span class="label label-success">Online</span>';
+ }
+ else
+ {
+  $status = '<span class="label label-danger">Offline</span>';
+ }
  $output .= '
  <tr>
   <td>'.$row['username'].'</td>
-  <td></td>
+  <td>'.$status.'</td>
   <td><button type="button" class="btn btn-info btn-xs start_chat" data-touserid="'.$row['user_id'].'" data-tousername="'.$row['username'].'">Start Chat</button></td>
  </tr>
  ';
