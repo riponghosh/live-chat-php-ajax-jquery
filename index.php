59a3b23f9ce595cd69610d28067f92557eb46016
riponghosh <ripon.ghosh200@gmail.com>
2018-07-26 23:43:26 +0600

    auto refresh chat message

--- index.php
+++ index.php
@@ -51,6 +51,7 @@ $(document).ready(function(){
  setInterval(function(){
   update_last_activity();
   fetch_user();
+  update_chat_history_data();
  }, 5000);
 
  function fetch_user()
@@ -79,6 +80,7 @@ $(document).ready(function(){
  {
   var modal_content = '<div id="user_dialog_'+to_user_id+'" class="user_dialog" title="You have chat with '+to_user_name+'">';
   modal_content += '<div style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;" class="chat_history" data-touserid="'+to_user_id+'" id="chat_history_'+to_user_id+'">';
+  modal_content += fetch_user_chat_history(to_user_id);
   modal_content += '</div>';
   modal_content += '<div class="form-group">';
   modal_content += '<textarea name="chat_message_'+to_user_id+'" id="chat_message_'+to_user_id+'" class="form-control"></textarea>';
@@ -112,6 +114,31 @@ $(document).ready(function(){
    }
   })
  });
+
+ function fetch_user_chat_history(to_user_id)
+ {
+  $.ajax({
+   url:"fetch_user_chat_history.php",
+   method:"POST",
+   data:{to_user_id:to_user_id},
+   success:function(data){
+    $('#chat_history_'+to_user_id).html(data);
+   }
+  })
+ }
+
+ function update_chat_history_data()
+ {
+  $('.chat_history').each(function(){
+   var to_user_id = $(this).data('touserid');
+   fetch_user_chat_history(to_user_id);
+  });
+ }
+
+ $(document).on('click', '.ui-button-icon', function(){
+  $('.user_dialog').dialog('destroy').remove();
+ });
  
 });  
 </script>
+
